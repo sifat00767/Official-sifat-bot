23ff1f57f6527029ec71e714a21c1f2c8b75a13c
sifat00767 <talha702427@gmail.com>
2026-08-31 15:56:12 +0600

    Update unsend.js

--- scripts/cmds/unsend.js
+++ scripts/cmds/unsend.js
@@ -14,7 +14,7 @@ module.exports = {
 
 	onStart: async function ({ message, event, api }) {
 		if (!event.messageReply || event.messageReply.senderID !== api.getCurrentUserID()) {
-			return message.reply(`⚠️ 𝗠𝗜𝗦𝗦𝗜𝗡𝗚 𝗥𝗘𝗣𝗟𝗬\n───────────────\n» ❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗽𝗹𝘆 𝘁𝗼 𝗮 𝗯𝗼𝘁 𝗺𝗲𝘀𝘀𝗮𝗴𝗲.\n───────────────\n» 👑 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 : 𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍`);
+			return message.reply(`⚠️ 𝗠𝗜𝗦𝗦𝗜𝗡𝗚 𝗥𝗘𝗣𝗟𝗬\n───────────────\n» ❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗽𝗹𝘆 𝘁𝗼 𝗮 𝗯𝗼𝘁 𝗺𝗲𝘀𝘀𝗮𝗴𝗲.\n───────────────\n» 👑 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 : _⁠-𝑨𝒅𝒎𝒊𝒏 𝑺𝒊𝒇𝒂𝒕 𝑺𝒊𝒓 ♡`);
 		}
 
 		message.unsend(event.messageReply.messageID);
@@ -28,7 +28,7 @@ module.exports = {
 
 		// বাংলা ও ইংরেজি মিক্সড কি-ওয়ার্ডস লিস্ট
 		const silent = [
-			"u", "uns", "r", "unsend", "s", "siyam",
+			"u", "uns", "r", "unsend", "s", "sifat",
 			"ডিলিট করো", "আনসেন্ট", "ডিলিট কর বট"
 		];
 
